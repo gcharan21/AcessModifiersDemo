1149446c331019d02e58a772bcf649b6d74ca976
GuruCharanReddy <gcharan881@gmail.com>
2021-08-24 08:38:46 +0530

    Add project files.

---
+++ AcessModifiersDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyBusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessModifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee { Id = 101, Name= "Guru charan" };
+            Console.WriteLine(employee);
+
+        }
+    }
+}
---
+++ MyBusinessLogic/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBusinessLogic
+{
+
+    internal class Address
+    {
+       
+        public string street { get; set; }
+        public string City { get; set; }
+        public int  Pincode { get; set; }
+    }
+    public class Employee
+
+    { 
+            public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+           return $"{Id }{Name }";
+        }
+
+    }
+    
+}
